183639d876ead497b90900c7379db025347a9c75
tiaditaa <118504168+tiaditaa@users.noreply.github.com>
2023-05-04 08:03:23 +0700

    Create display.h

---
+++ display.h
@@ -0,0 +1,10 @@
+#ifndef display_H
+#define display_H
+
+#include"header.h"
+
+//void (short x, short y); //fungsi untuk mengatur posisi kursor
+void Menu(); // menampilkan box menu
+void aturanDisplay(); // menampilkan aturan kerajaan
+
+#endif // display_H
